e7c2ae1103bc5e3afb025b60225d0bb339c70a1b
Santiago Maldonado Ochoa <santimaldonadoochoa@gmail.com>
2019-07-16 17:08:28 -0300

    Primer commit. Complete los metodos: 	FilaPreferencial: insertar, atender 	FilaGeneral: insertar, atender 	Cliente: modificarcategoria Los de insertar, son una linea que agrega un elemento a la lista del atributo fila: 	self.fila.append(cliente) El de modificarcategoria modifica el atributo categoria, con el valor del input: 	self.categoria = categoria

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
 class Fila(object):
     """Clase base de fila"""
-
     def __init__(self):
-         """constructor de la clase Fila """
-        self.enfila= 0
+        """constructor de la clase Fila """
+        self.enfila = 0
         self.fila = []
 
 class FilaPreferencial(Fila):
-    """Clase de la fila de los clientes preferenciales"""        
+    """Clase de la fila de los clientes preferenciales"""
 
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila preferencial"""
-        pass
+        self.fila.append(cliente)
 
     def atender(self):
-        """Atiende al proximo cliente prederencial"""
+        """Atiende al proximo cliente preferencial"""
         self.enfila-=1
         self.fila.pop(0)
-    
+
     def abrircajanueva(self,maxenfila,filanueva):
         """Si maxenfila es menor que la cantidad de clientes actualmente en espera, abro nueva caja"""
         pass
-    
-    
-    
+
+
+
 class FilaGeneral(Fila):
     """Clase que mantiene una fila de clientes no preferenciales"""
 
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila no preferencial"""
-        pass
+        self.fila.append(cliente)
 
     def atender(self):
-        """Atiende al proximo cliente prederencial"""
-        pass      
+        """Atiende al proximo cliente no preferencial"""
+        self.enfila-=1
+        self.fila.pop(0)
 
-    
 
 class cliente(object):
-     """clase cliente """
+    """clase cliente """
     def __init__(self,dni):
-         """ constructor de la clase cliente """
+        """ constructor de la clase cliente """
         self.dni=dni
         self.categoria=None
     def modificarcategoria(self, categoria):
         """modifica el atributo categoria del cliente """
-        pass
-  
-    
+        self.categoria = categoria
+
+
 if __name__ == "__main__":
     """ simular una fila en una entidad bancaria"""
     pass
